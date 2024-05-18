015158b37e97ffac688e32157fec85451b19260d
Upamanyu Kashyap <upamanyukashyap@jklu.edu.in>
2024-05-18 08:09:26 +0530

    Few lines of code added

---
+++ Array1.java
@@ -0,0 +1,16 @@
+public class Array1{
+    public static void main(String[] args) {
+        // int[] arr = new int[5];   //First way to create an array
+        // arr[0] = 10;
+        // arr[1] = 20;
+        // arr[2] = 30;
+        // arr[3] = 40;
+        // arr[4] = 50;
+        int[] arr= {10,20,30,40,50};    //Second way to create an array
+        System.out.println(arr[0]);
+        System.out.println(arr[1]);
+        System.out.println(arr[2]);
+        System.out.println(arr[3]);
+        System.out.println(arr[4]);
+    }
+}

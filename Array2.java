ffc425ef15ccf21d8c6f2f036ddf78cdefcece92
Upamanyu Kashyap <upamanyukashyap@jklu.edu.in>
2024-05-18 08:20:45 +0530

    Array of String

---
+++ Array2.java
@@ -0,0 +1,17 @@
+public class Array2 {
+    public static void main(String[] args){
+        //Array of string
+        String[] arr=new String[5];
+        arr[0]="Hello";
+        arr[1]="This";
+        arr[2]="is";
+        arr[3]="Upamanyu";
+        arr[4]="Kashyap";
+        System.out.println(arr[0]);
+        System.out.println(arr[1]);
+        System.out.println(arr[2]);
+        System.out.println(arr[3]);
+        System.out.println(arr[4]);
+
+    }
+}
